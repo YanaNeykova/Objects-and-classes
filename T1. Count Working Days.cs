9aa49494f7201ad238334e8b3b4f830fca556342
YanaNeykova <32368269+YanaNeykova@users.noreply.github.com>
2017-11-05 09:17:35 +0200

    Add files via upload

---
+++ T1. Count Working Days.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Object_and_classes___Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startDateEntry = Console.ReadLine();
+
+            var endDateEntry = Console.ReadLine();
+
+            var startDate = DateTime.ParseExact(startDateEntry, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(endDateEntry, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+
+            DateTime[] holidays = new DateTime[11];
+
+            holidays[0] = new DateTime(endDate.Year, 01, 01);
+            holidays[1] = new DateTime(endDate.Year, 03, 03);
+            holidays[2] = new DateTime(endDate.Year, 05, 01);
+            holidays[3] = new DateTime(endDate.Year, 05, 06);
+            holidays[4] = new DateTime(endDate.Year, 05, 24);
+            holidays[5] = new DateTime(endDate.Year, 09, 06);
+            holidays[6] = new DateTime(endDate.Year, 09, 22);
+            holidays[7] = new DateTime(endDate.Year, 11, 01);
+            holidays[8] = new DateTime(endDate.Year, 12, 24);
+            holidays[9] = new DateTime(endDate.Year, 12, 25);
+            holidays[10] = new DateTime(endDate.Year, 12, 26);
+
+
+
+            int workingDaysCounter = ((endDate - startDate).Days) + 1;
+
+            for (var currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+            {
+                var day = currentDate.DayOfWeek;
+                //bool isHoliday = holidays.Contains(currentDate);
+                //bool isSaturday = holidays.Contains(currentDate);
+                //bool isSunday = holidays.Contains(currentDate);
+                if (holidays.Contains(currentDate) || day.Equals(DayOfWeek.Saturday) || day.Equals(DayOfWeek.Sunday))
+                {
+                    workingDaysCounter--;
+                }
+            }
+            Console.WriteLine(workingDaysCounter);
+
+        }
+
+    }
+}
---
+++ T6. Book Library Modification.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T6.Book_Library_Modification
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string ISBN_Number { get; set; }
+        public double price { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var authorDate = new Dictionary<string, DateTime>();
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split().ToArray();
+                var dates = tokens[3].Split(".".ToCharArray()).ToArray();
+                var releaseDate = new DateTime
+                    (
+                        int.Parse(dates[2]),
+                        int.Parse(dates[1]),
+                        int.Parse(dates[0])
+                    );
+                var book = new Book()
+                {
+                    Title = tokens[0],
+                    Author = tokens[1],
+                    Publisher = tokens[2],
+                    ReleaseDate = releaseDate,
+                    ISBN_Number = tokens[4],
+                    price = double.Parse(tokens[5])
+                };
+
+                if (!authorDate.ContainsKey(book.Title))
+                {
+                    authorDate[book.Title] = new DateTime();
+                }
+                authorDate[book.Title] = book.ReleaseDate;
+            }
+            authorDate = authorDate.OrderBy(a => a.Value).ThenBy(a => a.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var dateToPrintAfterTokens = Console.ReadLine().Split('.').ToArray();
+            var dateToPrintAFter = new DateTime(
+                int.Parse(dateToPrintAfterTokens[2]),
+                int.Parse(dateToPrintAfterTokens[1]),
+                int.Parse(dateToPrintAfterTokens[0]));
+
+            foreach (var authorAndDate in authorDate)
+            {
+                var author = authorAndDate.Key;
+                var date = authorAndDate.Value;
+                if (dateToPrintAFter < date)
+                {
+                    Console.WriteLine($"{author} -> {date.ToString(@"dd.MM.yyyy")}");
+                }
+            }
+
+        }
+    }
+}
